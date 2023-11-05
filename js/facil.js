92cb66aca423eac60699c742d9b9378fbd24778c
jeongbin5211 <hjn123493@gmail.com>
2023-11-05 23:32:39 +0900

    1105

---
+++ js/facil.js
@@ -0,0 +1,39 @@
+document.querySelector('.facil-item-1').addEventListener('mouseover', (e) => {
+    e.preventDefault();
+    document.querySelector('.facil-item-1 p').style.visibility = 'visible';
+})
+
+document.querySelector('.facil-item-1').addEventListener('mouseleave', (e) => {
+    e.preventDefault();
+    document.querySelector('.facil-item-1 p').style.visibility = 'hidden';
+})
+
+document.querySelector('.facil-item-2').addEventListener('mouseover', (e) => {
+    e.preventDefault();
+    document.querySelector('.facil-item-2 p').style.visibility = 'visible';
+})
+
+document.querySelector('.facil-item-2').addEventListener('mouseleave', (e) => {
+    e.preventDefault();
+    document.querySelector('.facil-item-2 p').style.visibility = 'hidden';
+})
+
+document.querySelector('.facil-item-3').addEventListener('mouseover', (e) => {
+    e.preventDefault();
+    document.querySelector('.facil-item-3 p').style.visibility = 'visible';
+})
+
+document.querySelector('.facil-item-3').addEventListener('mouseleave', (e) => {
+    e.preventDefault();
+    document.querySelector('.facil-item-3 p').style.visibility = 'hidden';
+})
+
+document.querySelector('.facil-item-4').addEventListener('mouseover', (e) => {
+    e.preventDefault();
+    document.querySelector('.facil-item-4 p').style.visibility = 'visible';
+})
+
+document.querySelector('.facil-item-4').addEventListener('mouseleave', (e) => {
+    e.preventDefault();
+    document.querySelector('.facil-item-4 p').style.visibility = 'hidden';
+})
